b776c7e724978f301acb079daeaa04d3a3399bbf
rosesblake <rosesblake@yahoo.com>
2026-04-08 14:15:22 -0700

    added auto-loop for music

--- scenes/score.gd
+++ scenes/score.gd
@@ -4,7 +4,11 @@ func _ready():
 	Game.current_level_path = scene_file_path
 	Game.reset_level_score()
 	$CanvasLayer/Label.text = "%d" % Game.total_score
+	$Music.finished.connect(_on_music_finished)
 	$Music.play()
 
+func _on_music_finished():
+	$Music.play()
+	
 func _process(_delta):
 	$CanvasLayer/Label.text = "%d" % Game.total_score
